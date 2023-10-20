e490f8abaf5da9388c1671fec14db4dfd59ba901
Guille <g.mirandagaleote@gmail.com>
2023-10-20 09:00:18 +0200

    update testing

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 
 
 export const arrayLenght = (a) => {
+  if (a===String) {
+    a=String;
+    const toArray = a.split("");
+  }
+
   let count = 0;
-  // eslint-disable-next-line no-unused-vars
+
   for (const i of a) { 
     count++ 
 
   } 
 
   return count
+  
 };
 
-export const arrayPush = (b)=>{
-  let count = 0;
-  const newArray = b[count]
-
-  // eslint-disable-next-line no-unused-vars
-  for (const i of b) { 
-    count++ 
-
-  }
-
-;
-  
-  return newArray
-}
---
+++ save.js
@@ -0,0 +1,34 @@
+/*describe('arrayPush test', () => {
+  test('should be',()=>{
+    const list = [9,'u',1,11,3];
+    const expectedResult = [9,'u',1,11];
+    const result = arrayPush(list);
+  expect (result).toBe(expectedResult);
+  });
+});
+
+describe('given array push',() => {
+  test('should be', () => {
+    const data = [];
+    const result = arraypush();
+  })
+})
+export const arrayPush = (b)=>{
+  let count = 0;
+  const newArray = b[count]
+
+  // eslint-disable-next-line no-unused-vars
+  for (const i of b) { 
+    count++ 
+
+  }
+
+;
+  
+  return newArray
+}
+
+if (a !== Array) {
+  throw new Error('');
+}
+[9,'u',1,11,3]*/
